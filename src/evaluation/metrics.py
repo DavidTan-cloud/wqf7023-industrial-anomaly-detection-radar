375a9fdf05d98f6b9bbb34939d595c7226e19f32
DavidTan-cloud <25053589@siswa.um.edu.my>
2026-07-03 01:44:24 +0800

    Refactor evaluate function for metrics calculation

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,24 +1,48 @@
 from sklearn.metrics import (
+    accuracy_score,
     precision_score,
     recall_score,
     f1_score,
-    roc_auc_score,
-    accuracy_score
+    roc_auc_score
 )
 
-def evaluate(y_true,
-             y_pred,
-             anomaly_scores):
+def evaluate(
+    y_true,
+    y_pred,
+    scores
+):
 
     return {
         "Accuracy":
-            accuracy_score(y_true, y_pred),
+            accuracy_score(
+                y_true,
+                y_pred
+            ),
+
         "Precision":
-            precision_score(y_true, y_pred),
+            precision_score(
+                y_true,
+                y_pred,
+                zero_division=0
+            ),
+
         "Recall":
-            recall_score(y_true, y_pred),
+            recall_score(
+                y_true,
+                y_pred,
+                zero_division=0
+            ),
+
         "F1":
-            f1_score(y_true, y_pred),
+            f1_score(
+                y_true,
+                y_pred,
+                zero_division=0
+            ),
+
         "AUC":
-            roc_auc_score(y_true, anomaly_scores)
+            roc_auc_score(
+                y_true,
+                scores
+            )
     }
