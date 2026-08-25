900aa2cd994cfcd6466ed94906338a2febd1fa15
DavidTan-cloud <25053589@siswa.um.edu.my>
2026-08-25 13:35:33 +0800

    Setup project root path in mimii_test.py
    Add project root path setup to mimii_test.py

--- src/datasets/mimii_test.py
+++ src/datasets/mimii_test.py
@@ -1,3 +1,18 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "../.."
+    )
+)
+
+sys.path.insert(0, PROJECT_ROOT)
+
+print(PROJECT_ROOT)
+print(os.getcwd())
+
 from src.datasets.mimii_loader import MIMIILoader
 
 loader = MIMIILoader(
