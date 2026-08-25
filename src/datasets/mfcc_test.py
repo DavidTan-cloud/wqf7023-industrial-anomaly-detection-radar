5b57e3fb2ed06bf19ae80a765f5a0f0e454811c0
DavidTan-cloud <25053589@siswa.um.edu.my>
2026-08-25 13:42:35 +0800

    Add mfcc_test.py for MIMII feature extraction

---
+++ src/datasets/mfcc_test.py
@@ -0,0 +1,25 @@
+# src/datasets/test_mfcc.py
+
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "../.."
+    )
+)
+
+sys.path.insert(0, PROJECT_ROOT)
+
+from src.datasets.mimii_preprocessor import (
+    MIMIIPreprocessor
+)
+
+processor = MIMIIPreprocessor()
+
+features = processor.extract_features(
+    "src/datasets/raw/MIMII/0_dB_fan/fan/id_00/normal/normal_id_00_00000000.wav"
+)
+
+print(features.shape)
